e1689f983f6413fdd5137c3e98cc63694614054b
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-07-06 16:29:55 +0900

    working on view and fixed bug in create_account service

--- src/view/create/index.php
+++ src/view/create/index.php
@@ -3,4 +3,8 @@ $site_name .= " - new post";
 $page_description = "Post on XSN";
 ?>
 
+<div style="display:flex;align-items:center;justify-content:center;height:100%;width:100%">
+asd
+</div>
+
 <?php include_once("view/partial_navbar.php"); ?>
