6e67578937df5275aa40960fcf00cbf5809ada56
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-07-06 16:24:27 +0900

    changed view and improved navbar

--- src/view/partial_navbar.php
+++ src/view/partial_navbar.php
@@ -1,26 +1,30 @@
 <?php
-function if_active($potential_page) {
+function if_active($potential_pages) {
   global $page;
-  if ($page == $potential_page) {
-    echo "active";
+  
+  foreach ($potential_pages as $potential_page) {
+    if ($page == $potential_page) {
+      echo "active";
+      return;
+    }
   }
 }
 ?>
 
 <nav class="main-nav">
-    <a class="nav-icon <?php if_active("index"); ?>" href="?">
+    <a class="nav-icon <?php if_active(["index"]); ?>" href="?">
         <i class="material-icons">home</i>
     </a>
-    <a class="nav-icon <?php if_active("search"); ?>" href="?p=search">
+    <a class="nav-icon <?php if_active(["search"]); ?>" href="?p=search">
         <i class="material-icons">search</i>
     </a>
-    <a class="nav-icon <?php if_active("new_post"); ?>" href="?p=new_post">
+    <a class="nav-icon <?php if_active(["create"]); ?>" href="?p=create">
         <i class="material-icons">add</i>
     </a>
-    <a class="nav-icon <?php if_active("notifications"); ?>" href="?p=notifications">
+    <a class="nav-icon <?php if_active(["notifications"]); ?>" href="?p=notifications">
         <i class="material-icons">notifications</i>
     </a>
-    <a class="nav-icon <?php if_active("profile"); ?>" href="?p=profile">
+    <a class="nav-icon <?php if_active(["profile"]); ?>" href="?p=profile">
         <i class="material-icons">person</i>
     </a>
 </nav>
