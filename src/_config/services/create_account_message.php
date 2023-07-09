a0e4d84110749ee55fec7168e4f217b41a8d5378
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-07-09 20:15:47 +0900

    improved 'not loggedin' message

--- src/_config/services/create_account_message.php
+++ src/_config/services/create_account_message.php
@@ -4,7 +4,7 @@
 if (!isset($_SESSION["user_id"]) && (!isset($_GET['p']) || ($_GET['p'] != "register" && $_GET['p'] != "login"))) {
   echo "
     <div role='alert' style='padding:20px;background:#ffff0033'>
-      It looks like you haven't created an account yet. Please <a href='?p=register' style='color:#0000ff;text-decoration:underline'>create</a> an account to participate in XSN.
+      It looks like you haven't created an account yet. Please <a href='?p=register' style='color:#0000ff;text-decoration:underline'>create</a> an account or <a href='?p=login' style='color:#0000ff;text-decoration:underline'>login</a> to participate in XSN.
     </div>
   ";
 }
