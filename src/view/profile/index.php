c5359ea2413efee02d6be4f2677ae2886865d10d
m1dugh <romain103paris@gmail.com>
2023-07-13 11:10:11 +0900

    #4 feat(view): adds template for profile view

--- src/view/profile/index.php
+++ src/view/profile/index.php
@@ -1,6 +1,13 @@
 <?php
 $site_name .= " - profile";
 $page_description = "View your profile";
+
+include_once("controller/user.php");
 ?>
 
 <?php include_once("view/partial_navbar.php"); ?>
+
+<div>
+    Username: <?php echo $user["username"]; ?>
+    <!-- TODO: implement profile page -->
+</div>
