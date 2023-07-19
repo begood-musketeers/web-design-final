89e12c92e1b038f079d8c8bb00b970805e767d8f
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-07-20 00:18:16 +0900

    added notifications

--- src/model/comment_db.php
+++ src/model/comment_db.php
@@ -15,6 +15,23 @@ class CommentDB {
 
     public static function add_comment($user_id, $object_id, $object_type, $content) {
         $db = SimpleDB::Singleton();
+
+        // find the object owner
+        $sql = "
+        SELECT user_id
+        FROM $object_type
+        WHERE id = ?
+        ";
+        $user_result = $db->fetch_prepared($sql, "i", $object_id);
+
+        // notify the object owner
+        $sql = "
+        INSERT INTO notification (user_id, liking_user_id, object_id, object_type, type)
+        VALUES (?, ?, ?, ?, ?)
+        ";
+        $result = $db->query_prepared($sql, "iiiss", $user_result[0]['user_id'], $user_id, $object_id, $object_type, "comment");
+
+        // insert comment
         return $db->query_prepared("INSERT INTO comment (`user_id`, `object_id`, `object_type`, `content`) VALUES (?, ?, ?, ?)", "iiss", $user_id, $object_id, $object_type, $content);
     }
 
--- src/model/like_db.php
+++ src/model/like_db.php
@@ -15,13 +15,28 @@ class LikeDB {
   }
 
   public static function like_object($id, $type) {
-        $db = SimpleDB::Singleton();
+    $db = SimpleDB::Singleton();
     $user_id = $_SESSION['user_id'];
     $sql = "
     INSERT INTO user_like (user_id, object_id, object_type)
     VALUES ($user_id, $id, '$type')
     ";
     $result = $db->query($sql);
+
+    // find the object owner
+    $sql = "
+    SELECT user_id
+    FROM $type
+    WHERE id = ?
+    ";
+    $user_result = $db->fetch_prepared($sql, "i", $id);
+
+    // notify the object owner
+    $sql = "
+    INSERT INTO notification (user_id, liking_user_id, object_id, object_type, type)
+    VALUES (?, ?, ?, ?, ?)
+    ";
+    $result = $db->query_prepared($sql, "iiiss", $user_result[0]['user_id'], $user_id, $id, $type, "like");
   }
 
   public static function unlike_object($id, $type) {
