dacc0ca2e2980da409a756d0e271efe77a2f6f8c
R1ch4rd <32466820+DutchInnovation@users.noreply.github.com>
2023-07-20 01:08:03 +0900

    Bucket list front end

--- src/view/bucket_list/index.php
+++ src/view/bucket_list/index.php
@@ -5,28 +5,38 @@ $page_description = "Create a bucket list on XSN";
 include_once("controller/bucket_list.php");
 
 ?>
+<body class="gradient-a">
+    <div id="new_bucket_list-form" class="card shadow"> 
+<h1>New bucket list</h1>
 
 <?php
 if (is_array($bucket_lists) && count($bucket_lists) > 0) {
+    echo '<ul>'; // Opening the <ul> tag here
     foreach($bucket_lists as $bucket_list) {
         $id = $bucket_list["id"];
         $title = $bucket_list["title"];
 ?>
-    <li>
-        <h3><?php echo $title; ?></h3>
-        <form action="" method="GET">
-            <input type="hidden" name="p" value="new_bucket_list" />
-            <input type="hidden" name="bucket_list_id" value="<?php echo $id; ?>" />
-            <button type="submit">modify</button>
-        </form>
-        <form action="" method="POST">
-            <input type="hidden" name="request" value="delete_bucket_list" />
-            <input type="hidden" name="bucket_list_id" value="<?php echo $id; ?>" />
-            <button type="submit">delete</button>
-        </form>
-    </li>
+        <li>
+            <h3><?php echo $title; ?></h3>
+            <form action="" method="GET">
+                <input type="hidden" name="p" value="new_bucket_list" />
+                <input type="hidden" name="bucket_list_id" value="<?php echo $id; ?>" />
+                <button class="btn background-a text-white text-center pointer" type="submit">modify</button>
+            </form>
+            <form action="" method="POST">
+                <input type="hidden" name="request" value="delete_bucket_list" />
+                <input type="hidden" name="bucket_list_id" value="<?php echo $id; ?>" />
+                <button class="btn background-c text-white text-center pointer" type="submit">delete</button>
+            </form>
+        </li>
 <?php
     }
+    echo '</ul>'; // Closing the <ul> tag here
 }
 ?>
-</ul>
+</div>
+
+
+
+</body>
+<?php include_once("view/partial_navbar.php"); ?>
--- src/view/new_bucket_list/index.php
+++ src/view/new_bucket_list/index.php
@@ -1,55 +1,53 @@
 <?php
     include_once("controller/new_bucket_list.php");
 ?>
-
+<body>
 <?php
 if (isset($bucket_list_id)) {
     $bucket_list = BucketListDB::get_bucket_list($bucket_list_id);
     $name = BucketListDB::get_bucket_list_name($bucket_list_id);
 ?>
-    <h1><?php echo $name; ?></h1>
+
+<form action="" method="POST" class="gradient-a flex-center" style="height:100%;width:100%">
+        <div id="new_bucket_list-form" class="card shadow" style="width:100%;max-width:400px">
 <?php
+    echo "<h1 class='text-align'> $name" . " "  . 'Bucketlist' . "</h1><br>";
+    echo "<legend>Edit item<legend>";
     foreach($bucket_list as $key => $item) {
 ?>
-    <form action="" method="POST">
-        <legend>Edit item<legend>
-        <input type="hidden" value=<?php echo $bucket_list_id; ?> name="bucket_list_id" />
-        <input type="hidden" name="item_id" value=<?php echo $item["id"] ?> />
-        <input type="text" name="content" value="<?php echo $item["content"]; ?>" />
-        <input type="checkbox" name="completed" <?php echo $item["completed"] ? "checked" : "" ?> />
-        <input type="hidden" name="request" value="update_item" />
-        <button type="submit">submit</button>
-    </form>
-    <form action="" method="POST">
-        <input type="hidden" name="request" value="delete_item" />
-        <input type="hidden" value=<?php echo $bucket_list_id; ?> name="bucket_list_id" />
-        <input type="hidden" name="item_id" value=<?php echo $item["id"] ?> />
-        <button type="submit">delete</button>
+    <br>
+    <input type="hidden" value=<?php echo $bucket_list_id; ?> name="bucket_list_id" />
+    <input type="hidden" name="item_id" value=<?php echo $item["id"] ?> />
+    <input type="text" name="content" value="<?php echo $item["content"]; ?>" />
+    <input class="checkbox" type="checkbox" name="completed" <?php echo $item["completed"] ? "checked" : "" ?> />
+    <input type="hidden" name="request" value="update_item" />
+    <button class="btn background-a text-white text-center pointer" type="submit">complete</button>
+    <button class="btn background-c text-white text-center pointer" type="submit">delete</button> <br>
     </form>
 <?php
     }
 ?>
-
-<form action="" method="POST">
-    <legend>Add new item</legend>
+    <br><legend>Add new item</legend>
     <input type="hidden" value=<?php echo $bucket_list_id; ?> name="bucket_list_id" />
     <input type="text" name="content" />
     <input type="checkbox" name="completed" />
     <input type="hidden" name="request" value="add_item" />
-    <button type="submit">submit</button>
+    <button class="btn background-a text-white text-center pointer" type="submit">submit</button>
 </form>
-
 <?php
 } else {
 ?>
-
-<form action="" method="POST">
-    <input type="text" name="title" placeholder="bucket list title" />
-    <input type="hidden" name="request" value="new_bucket_list" />
-    <button type="submit">submit</button>
+<form action="" method="POST" class="gradient-a flex-center" style="height:100%;width:100%">
+        <div id="new_bucket_list-form" class="card shadow" style="width:100%;max-width:400px">
+            <img src="../../uploads/bucket.jpg" alt="Bucket" style="float:right;" width="150" height="150" />
+            <h1 class="text-align">New bucket list</h1><br>
+            <input  class="input-field" type="text" name="title" placeholder="bucket list title" />
+            <input type="hidden" name="request" value="new_bucket_list" />
+            <button class="btn background-a text-white text-center pointer" style="margin-top:10px;" type="submit">submit</button>
+        </div>
 </form>
-
 <?php
 }
 ?>
-
+</body>
+<?php include_once("view/partial_navbar.php"); ?>
